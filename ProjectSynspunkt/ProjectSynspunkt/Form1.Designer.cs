0e1d56ffd2a7dedf7992e647e7c83d09c42c44b0
Ludvig <128789599+LudvigPorsbjerg@users.noreply.github.com>
2023-05-15 09:48:49 +0200

    Project hype

--- ProjectSynspunkt/ProjectSynspunkt/Form1.Designer.cs
+++ ProjectSynspunkt/ProjectSynspunkt/Form1.Designer.cs
@@ -36,9 +36,10 @@
             this.label1.AutoSize = true;
             this.label1.Location = new System.Drawing.Point(367, 167);
             this.label1.Name = "label1";
-            this.label1.Size = new System.Drawing.Size(48, 13);
+            this.label1.Size = new System.Drawing.Size(68, 13);
             this.label1.TabIndex = 0;
-            this.label1.Text = "Fuck dig";
+            this.label1.Text = "Project Hype";
+            this.label1.Click += new System.EventHandler(this.label1_Click);
             // 
             // Form1
             // 
